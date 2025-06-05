944e87eb8607544a59cac51a2864c390941dbef7
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-06-05 08:06:12 +0200

    Use socket icmp ip protocol constants

--- yaping/protocol.py
+++ yaping/protocol.py
@@ -51,9 +51,6 @@ ICMP_DEFAULT_CODE = 0  # the code for ECHO_REPLY and ECHO_REQUEST
 ICMP_DEFAULT_SIZE = 64
 ICMP_DEFAULT_PAYLOAD = ICMP_DEFAULT_SIZE * b"Q"
 
-ICMP_PROTOCOL = socket.getprotobyname("icmp")
-ICMP_V6_PROTOCOL = socket.getprotobyname("ipv6-icmp")
-
 ECHO_REQUEST = 8
 ECHO_REPLY = 0
 ECHO_V6_REQUEST = 128
@@ -62,14 +59,14 @@ ECHO_V6_REPLY = 129
 
 class ICMPv4:
     family = socket.AF_INET
-    proto = ICMP_PROTOCOL
+    proto = socket.IPPROTO_ICMP
     ECHO_REQUEST = ECHO_REQUEST
     ECHO_REPLY = ECHO_REPLY
 
 
 class ICMPv6:
     family = socket.AF_INET6
-    proto = ICMP_V6_PROTOCOL
+    proto = socket.IPPROTO_ICMPV6
     ECHO_REQUEST = ECHO_V6_REQUEST
     ECHO_REPLY = ECHO_V6_REPLY
 
